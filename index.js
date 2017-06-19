0f199c91c76a61dc98219334f7729d00b8968623
nikersify <nikersifyyt@gmail.com>
2017-06-19 12:18:25 +0200

    move compiling logic to fig-compiler

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const babel = require('babel-core')
-const conv = require('convert-source-map')
+const compiler = require('fig-compiler')
 const path = require('path')
-const pug = require('pug')
-const sourceMap = require('source-map')
 const through = require('through2')
 
 const nameGen = require('./util/nameGen')
@@ -19,29 +16,14 @@ module.exports = file => {
 	if (EXTENSIONS.indexOf(extension) === -1) return through()
 
 	return through(function (buf, enc, next) {
-		const fileContents = buf.toString('utf8')
-		const lines = fileContents.split('\n')
+		const contents = buf.toString('utf8')
 
-		const tagContent = tag => {
-			let index = lines.findIndex(x => x.indexOf(tag) === 0)
-			if (index === -1) return null
-			index += 1
+		const compiled = compiler(contents, {
+			defaultName: nameGen(fileName)
+		})
 
-			let endIndex = lines.slice(index)
-				.findIndex(x => /^\S/.test(x))
-
-			let content
-			if (endIndex !== -1)
-				content = lines.slice(index, index + endIndex)
-			else
-				content = lines.slice(index)
-
-			const indentIndex = content[0].search(/\S/)
-			return content.map(x => x.slice(indentIndex)).join('\n')
-		}
-
-		const genExport = (name, str, quoted = true) => {
-			const p = 'module.exports.' + name + '='
+		const exported = (name, str, quoted = true) => {
+			const p = 'module.exporteds.' + name + '='
 			if (str === null) return p + 'null;'
 			else if (quoted) {
 				const val = str.split('\n')
@@ -52,23 +34,11 @@ module.exports = file => {
 			else return p + str + ';'
 		}
 
-		// template
-		const compiled = pug.compileClient(tagContent('template'))
-		this.push(compiled)
-		this.push(genExport('template', 'template\n', false))
-
-		// style
-		this.push(genExport('style', tagContent('style')))
-
-		// script
-		const scriptContent = tagContent('script')
-		const transformed = babel.transform(scriptContent, {
-			presets: require('babel-preset-es2015')
-		})
-		this.push(transformed.code)
-
-		// name
-		this.push(genExport('name', nameGen(fileName)))
+		this.push(compiled.template)
+		this.push(exported('template', 'template\n', false))
+		this.push(exported('style', compiled.style))
+		this.push(exported('name', compiled.name))
+		this.push(compiled.script)
 
 		next()
 	})
